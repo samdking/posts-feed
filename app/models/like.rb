8478dfea71cba0228708f8cf9349f3c030e7d879
Sam King <sdking02@gmail.com>
2022-05-19 22:09:07 +0100

    Add unit test for Like & improve validation message

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
   belongs_to :anonymous_user
 
-  validates :anonymous_user_id, uniqueness: { scope: :post_id }
+  validates :post_id, uniqueness: { scope: :anonymous_user_id, message: 'can only be liked once per user' }
 end
--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a user can only have 1 like per post" do
+    like = create(:like, post_id: 123)
+
+    assert_not build(:like, post_id: 123, anonymous_user: like.anonymous_user).valid?
+    assert create(:like, post_id: 456, anonymous_user: like.anonymous_user).valid?
+    assert create(:like, post_id: 123).valid?
+  end
 end
